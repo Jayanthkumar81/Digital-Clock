fd76f21c3c24e04972c99aaa6ede1be98f34d47e
Jayanth Kumar <125289275+Jayanthkumar81@users.noreply.github.com>
2025-07-20 10:45:04 +0530

    Add files via upload

---
+++ Digital Clock/JS/Clock.js
@@ -0,0 +1,15 @@
+let day=document.getElementById("day")
+let hrs=document.getElementById("hrs");
+let min=document.getElementById("min");
+let sec=document.getElementById("sec");
+
+
+setInterval(()=>{
+   let currentTime=new Date();
+   
+   day.innerHTML=(currentTime.getDate()<10?"0":"")+currentTime.getDate();
+   hrs.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getHours();
+   min.innerHTML=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+   sec.innerHTML=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+
+},1000)
